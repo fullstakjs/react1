b4c1ecacb5329f904c39c20617eca20f12a8385b
Joash Nyabuti <nyabutijoash@yahoo.com>
2021-02-22 17:03:20 -0600

    modified App.js with state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-
+import { useState } from "react";
 import './App.css';
+const whatTodo=[
+  {
+    id:0,
+    name: "Learn Js",
+    priority:1
+  },
+  {
+    id:0,
+    name: "Learn Angular",
+    priority:3
+  },
+  {
+    id:0,
+    name: "Learn React",
+    priority:2
+  }
+]
 
 function App() {
+  const [todos,setTodos]= useState(whatTodo);
+  const [todo, settodo] = useState("");
+
   return (
     <div className="App">
-      <p>Hello world</p>
+      <div className="form">
+        <h1>Add your todo list</h1>
+         <form >
+           <input type="text"/>
+           <input type="text"/>
+           <button type="submit">Add todo</button>
+         </form>
+      </div>
+      <div className="content">
+       {todos.map((doc)=>{
+           return(
+             
+               <p key={doc.id}>{doc.name} and priority of {doc.priority}</p>
+             
+           )
+       })}
+      </div>
     </div>
   );
 }
