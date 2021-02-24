d6ee5663863d7fb63223d466289fde08b4aa77a5
Joash Nyabuti <nyabutijoash@yahoo.com>
2021-02-23 23:14:33 -0600

    modified app.js file

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ const whatTodo=[
     priority:1
   },
   {
-    id:0,
+    id:1,
     name: "Learn Angular",
     priority:3
   },
   {
-    id:0,
+    id:2,
     name: "Learn React",
     priority:2
   }
@@ -20,26 +20,46 @@ const whatTodo=[
 
 function App() {
   const [todos,setTodos]= useState(whatTodo);
-  const [todo, settodo] = useState("");
+  const [todo, settodo] = useState({
+    tname:"",
+    priority:""
+  });
+
+  function handleChange(e){
+    const value=e.target.value;
+
+    settodo({
+      ...todo,
+      [e.target.name]: value,
+      [e.target.pri]:value
+
+    })
+
+    console.log(todo)
+  
+  }
+  function handleSubmit(e){
+  
+  }
 
   return (
     <div className="App">
       <div className="form">
         <h1>Add your todo list</h1>
-         <form >
-           <input type="text"/>
-           <input type="text"/>
+         <form onSubmit={handleSubmit}>
+           <input name="tname" type="text" value={todo.tname} onChange={handleChange}/>
+           <input name="pri" type="text" value={todo.priority} onChange={handleChange}/>
            <button type="submit">Add todo</button>
          </form>
       </div>
       <div className="content">
-       {todos.map((doc)=>{
+       {/* {todos.map((doc)=>{
            return(
              
                <p key={doc.id}>{doc.name} and priority of {doc.priority}</p>
              
            )
-       })}
+       })} */}
       </div>
     </div>
   );
